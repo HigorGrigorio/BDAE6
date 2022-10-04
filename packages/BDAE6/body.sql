8b9fdd0215de79ab01383e02cadefcd2bf9e014e
higor <higorgrigorio@gmail.com>
2022-10-04 10:31:54 -0300

    implemented procedure to insert into categoria

---
+++ packages/BDAE6/body.sql
@@ -0,0 +1,27 @@
+CREATE OR REPLACE PACKAGE BODY BDAE6 IS
+    PROCEDURE INSERT_CATEGORIA_IF_NOT_EXIST(
+        RV_NOME IN VARCHAR2,
+        RV_SIGLA IN VARCHAR2
+    ) AS
+        L_COUNT NUMBER;
+    BEGIN
+ -- check if the values already exist in the database.
+        SELECT
+            COUNT(1) INTO L_COUNT
+        FROM
+            CATEGORIA
+        WHERE
+            UPPER(UTILITY.REMOVE_ACCENTS(NOME)) = UPPER(UTILITY.REMOVE_ACCENTS(RV_NOME))
+            AND UPPER(UTILITY.REMOVE_ACCENTS(SIGLA)) = UPPER(UTILITY.REMOVE_ACCENTS(RV_SIGLA));
+ --
+        IF L_COUNT = 0 THEN
+            INSERT INTO CATEGORIA(
+                NOME,
+                SIGLA
+            ) VALUES (
+                RV_NOME,
+                RV_SIGLA
+            );
+        END IF;
+    END;
+END;
--- packages/utility/pkg.sql
+++ packages/utility/pkg.sql
@@ -1,14 +1,5 @@
 CREATE OR REPLACE PACKAGE UTILITY IS
  /*
-    Checks if has value into reference into table.  
-    Returns 1 if reference exists, 0 otherwise.
-*/
-    FUNCTION HAS_VALUE(
-        RV_TABLE IN VARCHAR2,
-        RV_FIELD IN VARCHAR2,
-        RV_VALUE IN VARCHAR2
-    ) RETURN NUMBER;
- /*
     */
     FUNCTION EXPLODE(
         RV_STRING IN VARCHAR2,
